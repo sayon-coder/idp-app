2deddc91a13b0e7e5e142ae8e7c2927548c53db6
sayon-coder <developwithsb@gmail.com>
2026-08-26 05:54:51 +0000

    feat: add terraform EKS cluster configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,7 +19,7 @@ variable "cluster_version" {
 variable "node_instance_type" {
   description = "EC2 instance type for worker nodes"
   type        = string
-  default     = "t3.medium"
+  default     = "t3.small"
 }
 
 variable "node_desired_size" {
